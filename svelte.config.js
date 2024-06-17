5fb704636127619b76ca6e784a67199084f52171
PRINCE KUMAR <86905668+princekumarg@users.noreply.github.com>
2024-06-17 04:30:48 +0000

    commit made on HMR PORT Changes #3

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+  vite: {
+    server: {
+      hmr: {
+        clientPort: process.env.HMR_HOST ? 443 : 5173,
+        host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : 'localhost'
+      }
+    }
+  },
   preprocess: vitePreprocess(),
 }
